fadc2c1d699e364524b9b672a5589f72ca720f3e
Vlad Flore <flore.vlad@gmail.com>
2025-10-24 01:45:15 +0200

    Refactor

--- examples/weaviate_query.py
+++ examples/weaviate_query.py
@@ -1,14 +1,21 @@
 import weaviate
 import json
 import sys
+from weaviate.collections.classes.grpc import MetadataQuery
+
 
 client = weaviate.connect_to_local()
 
 questions = client.collections.use("Blog")
 
-response = questions.query.near_text(query=sys.argv[1], limit=3)
+response = questions.query.near_text(
+    query=sys.argv[1],
+    limit=10,
+    return_metadata=MetadataQuery(distance=True),
+)
 
 for obj in response.objects:
     print(json.dumps(obj.properties, indent=2))
+    print(f"Distance to query: {obj.metadata.distance:.3f}")
 
 client.close()
